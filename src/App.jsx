5b2aa14aed63e8a678b619424e287964bc3640bd
Ahmed Ayman <110921228+AhmedArafat20@users.noreply.github.com>
2025-04-13 12:39:07 +0200

    gra

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import MyNavbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <MyNavbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
