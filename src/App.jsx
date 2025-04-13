82175442338352913b8651c12ceeced5ba66338b
Ahmed Ayman <110921228+AhmedArafat20@users.noreply.github.com>
2025-04-13 13:24:34 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
 import MyNavbar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer"; 
 
 function App() {
   return (
     <div>
       <MyNavbar />
       <Home />
+      <Footer /> 
     </div>
   );
 }
